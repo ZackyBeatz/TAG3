cdc5a82c12f776e823c1940f6f13a88638dd1e4b
BenedikteEva <cph-bm109@cphbusiness.dk>
2017-04-04 14:37:59 +0200

    Merge origin/master
    Conflicts:
    	TagVer3/TagVer3/src/tagver3/Armor.java

--- TagGameNew/src/tag2ver4/ItemsFactories/WeaponsFactory.java
+++ TagGameNew/src/tag2ver4/ItemsFactories/WeaponsFactory.java
@@ -14,7 +14,7 @@ public class WeaponsFactory {
     
         public Weapons[] defineWeapons() {
             
-        Weapons[] defineWeapons = new Weapons[2];
+        Weapons[] defineWeapons = new Weapons[5];
 
         defineWeapons[0] = new Weapons(1, " is a standard mele weapon.", "Sword");
         defineWeapons[0].setDamage(4);
@@ -22,14 +22,14 @@ public class WeaponsFactory {
         defineWeapons[1] = new Weapons(2, " A powerful rocket launcher wich does a heavy amount of damage.", "Patriot Rocket Launcher");
         defineWeapons[1].setDamage(6);
 
-        defineWeapons[3] = new Weapons(4, " An Arrow that fires a deadly chemical strike.", "Poison Arrow");
-        defineWeapons[3].setDamage(6);
+        defineWeapons[2] = new Weapons(4, " An Arrow that fires a deadly chemical strike.", "Poison Arrow");
+        defineWeapons[2].setDamage(6);
 
-        defineWeapons[4] = new Weapons(5, " A strong electricity Attack that can kill an enemy monster with a single shot.", "lightning thunder strike");
-        defineWeapons[4].setDamage(8);
+        defineWeapons[3] = new Weapons(5, " A strong electricity Attack that can kill an enemy monster with a single shot.", "lightning thunder strike");
+        defineWeapons[3].setDamage(8);
         
-        defineWeapons[5] = new Weapons(6, " A Rifle that can aim and shot at a monster's most valnuable place, ex. right at it's head.", "Sniper Rifle");
-        defineWeapons[5].setDamage(10);
+        defineWeapons[4] = new Weapons(6, " A Rifle that can aim and shot at a monster's most valnuable place, ex. right at it's head.", "Sniper Rifle");
+        defineWeapons[4].setDamage(10);
         
         return defineWeapons;
 
