3eb27a34b6decb841f4c955662ac0b33274494f6
Zakaria Ahmed <zacky89@hotmail.com>
2017-04-19 14:36:20 +0200

    "Har tilføjet HighScores"

---
+++ TagGameNew/src/tag2ver4/Score.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tag2ver4;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Zack
+ */
+public class Score  implements Serializable{
+    
+    private int score;
+    private String name;
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Score(String name, int score) {
+        this.score = score;
+        this.name = name;
+    }
+    
+}
--- TagGameNew/src/tag2ver4/TextBasedGame.java
+++ TagGameNew/src/tag2ver4/TextBasedGame.java
@@ -10,16 +10,38 @@ public class TextBasedGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
+        
         Room[][] roomMatrix = new RoomFactory().createRooms();
         GameController g = new GameController();
         Boundery b = new Boundery();
         String name = b.askNamed();
         Player n1 = new Player(name, 100, roomMatrix[0][0], 1, 0);
-
         b.intro(n1);
         g.StartGame(n1);
 //        g.runGame(n1);
 
+
+        // Highscores
+        HighScoreManager hm = new HighScoreManager();
+        // VI KAN EVT. LAVE ET ARRAY AF SPILLERE, SOM BLIR ADDED TIL VORES HIGH SCORE
+        // LIGENU ER DER KUN EN SPILLER
+        hm.addScore(n1.getName(), n1.getPlayerGold());
+        
+        /*
+        --------------------------------------------------------
+        ------------FOR AT PRINTE EN HIGHSCORE -----------------
+        hm.addScore("Bart",734);
+        hm.addScore("Marge",530);
+        hm.addScore("Maggie", 320);
+        hm.addScore("Homer",700);
+        hm.addScore("Lisa",270);
+
+        System.out.print(hm.getHighscoreString());
+        */
+        
+        
     }
 
 }
