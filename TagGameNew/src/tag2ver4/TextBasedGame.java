4911af642835e5eb2616f7289dc0840cd1a75ee7
Zakaria Ahmed <zacky89@hotmail.com>
2017-04-20 14:15:47 +0200

    "highscore der virker"

--- TagGameNew/src/tag2ver4/TextBasedGame.java
+++ TagGameNew/src/tag2ver4/TextBasedGame.java
@@ -1,5 +1,9 @@
 package tag2ver4;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author Zack
@@ -12,14 +16,14 @@ public class TextBasedGame {
     public static void main(String[] args) {
         
         
-        
-        Room[][] roomMatrix = new RoomFactory().createRooms();
-        GameController g = new GameController();
-        Boundery b = new Boundery();
-        String name = b.askNamed();
-        Player n1 = new Player(name, 100, roomMatrix[0][0], 1, 0);
-        b.intro(n1);
-        g.StartGame(n1);
+//        
+//        Room[][] roomMatrix = new RoomFactory().createRooms();
+//        GameController g = new GameController();
+//        Boundery b = new Boundery();
+//        String name = b.askNamed();
+//        Player n1 = new Player(name, 100, roomMatrix[0][0], 1, 0);
+//        b.intro(n1);
+//        g.StartGame(n1);
 //        g.runGame(n1);
 
 
@@ -27,7 +31,14 @@ public class TextBasedGame {
         HighScoreManager hm = new HighScoreManager();
         // VI KAN EVT. LAVE ET ARRAY AF SPILLERE, SOM BLIR ADDED TIL VORES HIGH SCORE
         // LIGENU ER DER KUN EN SPILLER
-        hm.addScore(n1.getName(), n1.getPlayerGold());
+        hm.addScore("Bart", 14349);
+        hm.addScore("sdart", 21223);
+        hm.addScore("Badrt", 45303);
+        hm.addScore("ast", 26513);
+        
+        
+        printMethod(hm);
+        
         
         /*
         --------------------------------------------------------
@@ -43,5 +54,19 @@ public class TextBasedGame {
         
         
     }
+    
+     public static void printMethod(HighScoreManager hm) {
+        try {
+            String file = "C:\\Users\\Zack\\Documents\\TAG3\\TagGameNew\\src\\tag2ver4\\scores.dat";
+            String text = hm.getHighscoreString();
+            try (BufferedWriter out = new BufferedWriter(new FileWriter(file))) {
+                out.write(text);
+            }
+        } catch (IOException out) {
+            System.out.print(out);
+        }
+
+    }
+
 
 }
